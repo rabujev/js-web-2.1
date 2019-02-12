099b60644d29cbb3e85944816f8a870a481aa327
Jamal Rabujev <jamal.rabujev@gmail.com>
2019-02-12 09:56:07 +0100

    2.1 done

--- script.js
+++ script.js
@@ -9,4 +9,11 @@ import 'bootstrap';
   Put the JavaScript code you want below.
 */
 
-console.log("Hey look in your browser console. It works!");
+import axios from 'axios';
+
+let imgTag = document.querySelector('#theImg');
+async function getRandCatImgWithApi() {
+  let randImg = await axios.get('https://aws.random.cat/meow');
+  imgTag.src = randImg.data.file;
+}
+setInterval (getRandCatImgWithApi, 1000);
